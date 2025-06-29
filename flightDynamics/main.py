20daaba1534755d1388576a771bbb2a752edd295
kuhnpi@mail.uc.edu <kuhnpi@mail.uc.edu>
2025-06-29 12:56:56 -0600

    cleaning up

--- flightDynamics/main.py
+++ flightDynamics/main.py
@@ -10,15 +10,14 @@ from dynamics import dynamics
 from plotting import plotting
 from animation import animation
 
-customState = np.array([[0,0,0,0,0,0,np.radians(90),np.radians(0),np.radians(40),0,0,0]]).T
-
+#Initialize Classes
 D = dynamics()
 P = params()
 Pl = plotting()
 A = animation(P.state0)
 timeSteps = int(P.T_end/P.Ts)
 time = 0
-u_star = P.u_star
+u_star = P.u_star #Create trim states
 
 #Set up storage for states
 stateStorage = np.zeros((timeSteps,len(D.state[:,0])+1))
@@ -26,13 +25,18 @@ FaMStorage = np.zeros((timeSteps,7))
 
 for i in range(timeSteps):
 
+    #Update the dynamics
     D.update(u_star)
 
+    #update the animation
     if i % P.plot_delimination == 0:
         A.update(D.state, time)
+        
+        #Kill the animation if I want
         if A.killFlag:
             break
     
+    #Store state variables
     stateStorage[i] = np.concatenate((D.state.flatten(), [time]))
     FaMStorage[i] = np.concatenate((np.concatenate((P.R_body_to_inertial(D.state.item(6),D.state.item(7),D.state.item(8))@D.F.flatten(),D.M.flatten())), [time]))
     time+=P.Ts
